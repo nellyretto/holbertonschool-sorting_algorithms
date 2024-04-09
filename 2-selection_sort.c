5272a9c21a892901f6700dc4feaa78a08f9570f1
Vonzell Carson <vonzell@LAPTOP-109H1J4B>
2024-04-09 14:15:37 -0400

    fixed file 2-selection_sort.c it should now be able to sort an array of integers in ascending order using the Selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -28,6 +28,7 @@ void selection_sort(int *array, size_t size)
 		if (min_idx != i)
 		{
 			temp = array[i];
+			array[i] = array[min_idx];
 			array[min_idx] = temp;
 			print_array(array, size);
 		}
