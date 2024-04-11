6ea86daa1784a0cb54dd57d49f7e30d4a977084d
Nelly <nelly@Nellys.local>
2024-04-10 22:23:46 -0400

    Changing method of compilation

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,45 +3,41 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorting a double linked list on ascending
- * @list: Pointer to a pointer to the head of the list
+ * insertion_sort_list - function that sorts a doubly linked list
+ * of integers in ascending order using the Insertion sort algorithm
+ * @list: Double linked list to sort
  */
 
 void insertion_sort_list(listint_t **list)
-
 {
-	listint_t *current;
-	listint_t *temp;
+	listint_t *current, *previous;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	current = (*list)->next;
-
 	while (current != NULL)
 	{
-		temp = current->prev;
-
-		while (temp != NULL && temp->n > current->n)
+		previous = current->prev;
+		while (previous != NULL && previous->n > current->n)
 		{
-			if (temp->prev != NULL)
-				temp->prev->next = current;
+			previous->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = previous;
+
+			current->prev = previous->prev;
+			if (previous->prev != NULL)
+				previous->prev->next = current;
 			else
 				*list = current;
 
-			if (current->next != NULL)
-				current->next->prev = temp;
+			current->next = previous;
+			previous->prev = current;
 
-			temp->next = current->next;
-			current->prev = temp->prev;
-			current->next = temp;
-			temp->prev = current;
+			previous = current->prev;
 
-			temp = current->prev;
+			print_list(*list);
 		}
-
-		print_list(*list);
-
 		current = current->next;
 	}
 }
